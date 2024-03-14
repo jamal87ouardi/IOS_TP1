a77005afe992eb46c77fd6cc514229777810616f
Ouardi <ouardi@192.168.1.116>
2024-03-14 16:00:48 +0000

    first

---
+++ main.swift
@@ -0,0 +1,24 @@
+//
+//  main.swift
+//  M207_tp1
+//
+//  Created by Ouardi on 14/3/2024.
+//
+
+import Foundation
+
+print("Entrer N")
+
+var N = Double(readLine()!)!
+
+// Traitements
+
+var resultat = sqrt(N)
+
+var s = sin(N)
+
+
+// Output
+print("La racine carée de \(N) est \(resultat)")
+
+print("La sinus de \(N) est \(s)")
